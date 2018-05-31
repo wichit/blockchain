18eb2ae99ceaa0290e93419b882fdf7a3c42b8ac
Nattapon Nimakul <totizy@gmail.com>
2018-06-01 00:00:52 +0700

    [Smart contract] add signature.sol

---
+++ KATIN Smart contract/helper/signature/signature.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.4.24;
+
+contract Verifier {
+    function splitSignature(bytes sig)
+    public
+    pure
+    returns (uint8, bytes32, bytes32)
+    {
+        require(sig.length == 65);
+    
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+    
+        assembly {
+            // first 32 bytes, after the length prefix
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+    
+        return (v, r, s);
+    }
+    
+    function recoverAddr(bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) returns (address) {
+        return ecrecover(msgHash, v, r, s);
+    }
+    
+    function isSigned(address _addr, bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) returns (bool) {
+        return ecrecover(msgHash, v, r, s) == _addr;
+    }
+
+    function verifySigned(address _addr, bytes32 msgHash, bytes sig)
+        public
+        pure
+        returns (bool)
+    {
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+    
+        (v, r, s) = splitSignature(sig);
+    
+        return ecrecover(msgHash, v, r, s) == _addr;
+    }
+}
