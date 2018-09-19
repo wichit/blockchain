e47fb42a52915547a999bdf43dcd335c60324aa4
Wichit Saiklao <wichit@dxplace.com>
2018-09-19 12:27:57 +0700

    Simple Lottery
    Simple lottery

---
+++ simplelottery.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.0;
+contract SimpleLottery {
+    uint public constant TICKET_PRICE = 1e16; // 0.01 ether
+    
+    address[] public tickets;
+    address public winner;
+    uint public ticketingCloses;
+    uint public ticketSold;
+    
+    function SimpleLottery (uint duration) public {
+        ticketingCloses = now + duration;
+        ticketSold = 0;
+    }
+    
+    function buy () public payable {
+        require(msg.value == TICKET_PRICE);
+        require(now < ticketingCloses);
+        
+        tickets.push(msg.sender);
+        ticketSold++;
+    }
+    
+    function drawWinner () public {
+        require(now > ticketingCloses + 3 minutes);
+        require(winner == address(0));
+        
+        bytes32 rand = keccak256(block.blockhash(block.number-1));
+        
+        winner = tickets[uint(rand) % tickets.length];
+    }
+    
+    function withdraw () public {
+        require(msg.sender == winner);
+        msg.sender.transfer(this.balance);
+    }
+    
+    function () payable public {
+        buy();
+    }
+}
