4ad64f95fba1a2b8d2d801019d8530ed8112df30
Thanwa JDR <thanwa.jdr@gmail.com>
2018-10-01 14:33:03 +0700

    add message on require statement

--- KATIN Smart contract/KATINCoin.sol
+++ KATIN Smart contract/KATINCoin.sol
@@ -135,7 +135,7 @@ contract StandardToken is BasicToken, ERC20 {
         returns (bool success)
     {
         // if the allowance isn't 0, it can only be updated to 0 to prevent an allowance change immediately after withdrawal
-        require(_value == 0 || allowance[msg.sender][_spender] == 0);
+        require(_value == 0 || allowance[msg.sender][_spender] == 0, "value not equals 0 or spender of allowance not equals 0");
 
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
